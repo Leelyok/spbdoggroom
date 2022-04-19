7a9d377c8c87ce1601fae7b3db12a5fc76d350e0
Lily <89401279+Leelyok@users.noreply.github.com>
2022-04-19 16:50:05 +0300

    add some uppd

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ $('.hamburger-button').click(function(){
 
 
   $('.testimon-slider-container').slick({
-    autoplay: true,        // Do we want it to autoplay? true or false
+    autoplay: false,        // Do we want it to autoplay? true or false
     autoplaySpeed: 3000,   // How long between each slide when autoplaying
     speed: 500,            // How fast is the transition 
     arrows: false,          // Do you want to show arrows to trigger each slide
@@ -31,7 +31,7 @@ $('.hamburger-button').click(function(){
   });
   
   $(".testimonial-slider-container").slick({
-      autoplay: true,
+      autoplay: false,
       autoplaySpeed: 2e3,
       speed: 500,
       arrows: true,
